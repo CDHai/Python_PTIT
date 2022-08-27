53550c8d51c6e07f5375e32aef7ae88f21c50df7
Cao Duy Hai <88284121+CDHai@users.noreply.github.com>
2022-08-27 11:38:11 +0700

    Add files via upload

---
+++ py01006.py
@@ -0,0 +1,15 @@
+t = int(input())
+while t>0:
+        d = 1
+        s = input()
+        for i in s:
+            if(i != '4' and i != '7'):
+                d = 0
+                break
+        if d==0:
+            print("NO")
+        else:
+            print("YES")
+        t -= 1
+
+
