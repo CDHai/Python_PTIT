0377a0205bb53a456324c8fb4e0ea422cbd04cca
Cao Duy Hai <88284121+CDHai@users.noreply.github.com>
2022-08-27 11:39:14 +0700

    Add files via upload

---
+++ py01007.py
@@ -0,0 +1,10 @@
+t = int(input())
+while(t>0):
+    t -= 1
+    d = 0
+    n,x,m = [float(x) for x in input().split()]
+    m /= n
+    while(m>=1):
+        m = m/(1+x/100)
+        d += 1
+    print(d)
