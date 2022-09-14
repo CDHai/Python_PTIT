58c5dd129557757be2f19047b7f5f7f2e7e6d0ff
Cao Duy Hai <88284121+CDHai@users.noreply.github.com>
2022-09-14 23:05:57 +0700

    Add files via upload

---
+++ py01018.py
@@ -0,0 +1,19 @@
+p = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_."
+while True:
+    str = input()
+    if(str == '0'): 
+        break
+    else:
+        k, s = str.split()
+        k = int(k)
+        n = ""
+        for i in s:
+            x = 0
+            for j in p:
+                if i == j: 
+                    break
+                x += 1
+            x = (x + k) % 28
+            n = p[x] + n
+        print(n)
+        
